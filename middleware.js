8f4664cda4530e4b42fce51a58ceb4f0c1dc087d
Pham Anh Khoa <phamanhkhoa1609@gmail.com>
2025-03-14 08:54:32 +0700

    get token

---
+++ middleware.js
@@ -0,0 +1,9 @@
+export function middleware(request) {
+    
+}
+
+export const config = {
+    matcher: [
+      '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+  }
---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+const config = {
+    "quoteProps": "preserve",
+};
+
+export default config;
